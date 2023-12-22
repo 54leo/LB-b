3cdea62408be70cfddfdfe2d4e3e3ba276b21f67
leo roth <leoroth2008@gmail.comg>
2023-12-22 10:42:06 +0000

    chmmit 2

--- Code-lb.js/LB-b.js
+++ Code-lb.js/LB-b.js
@@ -1,90 +1,86 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // CORS-Modul hinzugefügt
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.json());
 
-// CORS-Konfiguration hinzugefügt
 app.use(cors({
   origin: 'http://localhost:8080',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
-
-const tasks = [];
+//the whole array was created by chatGPT
+let tasks = [
+  { task: "Einkauf erledigen", autor: "Max Mustermann", creaDate: "2023-01-15", finDate: "2023-01-18" },
+  { task: "Präsentation vorbereiten", autor: "Anna Beispiel", creaDate: "2023-02-01", finDate: "2023-02-10" },
+  { task: "Sport treiben", autor: "Erika Musterfrau", creaDate: "2023-03-05", finDate: "2023-03-07" },
+  { task: "Buch lesen", autor: "Tom Testperson", creaDate: "2023-04-10", finDate: "2023-04-20" },
+  { task: "Projekt abschließen", autor: "Lisa Proben", creaDate: "2023-05-15", finDate: "2023-06-01" },
+  { task: "Reise planen", autor: "Paul Planer", creaDate: "2023-07-02", finDate: "2023-07-15" },
+  { task: "Geburtstagsgeschenk kaufen", autor: "Alex Aufmerksam", creaDate: "2023-08-20", finDate: "2023-08-25" },
+  { task: "Sprachkurs beginnen", autor: "Sarah Sprachliebhaber", creaDate: "2023-09-10", finDate: "2023-10-01" },
+  { task: "Website aktualisieren", autor: "Chris Coder", creaDate: "2023-11-05", finDate: "2023-11-15" },
+  { task: "Familientreffen organisieren", autor: "Fiona Familienmensch", creaDate: "2023-12-01", finDate: "2023-12-10" }
+];
 
 app.get('/tasks', (req, res) => {
   res.json({ tasks });
 });
 
 app.post('/tasks', (req, res) => {
-  try {
-    const { title, description } = req.body;
+  const { task, autor, creaDate, finDate } = req.body;
 
-    // Validierung für Benutzereingaben hinzugefügt
-    if (!title || !description) {
-      return res.status(400).json({ error: 'Title and description are required' });
-    }
-
-    const newTask = {
-      id: tasks.length + 1,
-      title,
-      description
-    };
-    tasks.push(newTask);
-
-    // Logging hinzugefügt
-    console.log(`New task created: ${newTask.title}`);
-
-    res.status(201).json({ task: newTask });
-  } catch (error) {
-    // Fehlerbehandlung hinzugefügt
-    console.error('Error creating task:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+  if (!task || !autor || !creaDate || !finDate) {
+    return res.status(400).json({ error: 'All fields are required' });
   }
+//help from chatGPT because code didnt work
+  const newTask = {
+    id: tasks.length + 1,
+    task,
+    autor,
+    creaDate,
+    finDate,
+  };
+
+  tasks.push(newTask);
+  res.status(201).json({ message: 'Task created successfully', task: newTask });
 });
 
 app.get('/tasks/:id', (req, res) => {
   const taskId = parseInt(req.params.id);
   const task = tasks.find(task => task.id === taskId);
+
   if (!task) {
-    res.status(404).json({ error: 'Task not found' });
-  } else {
-    res.json({ task });
+    return res.status(404).json({ error: 'Task not found' });
   }
+
+  res.json({ task });
 });
 
 app.patch('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
-  const task = tasks.find(task => task.id === taskId);
-  if (!task) {
-    res.status(404).json({ error: 'Task not found' });
-  } else {
-    Object.assign(task, req.body);
-    res.json({ task });
+  const keys = Object.keys(req.body);
+  const changeTask = tasks.find(task => task.id === parseInt(req.params.id));
+
+  if (!changeTask) {
+    return res.status(404).json({ error: 'Task not found' });
   }
+
+  keys.forEach(key => {
+    changeTask[key] = req.body[key];
+  });
+
+  res.json({ message: 'Task updated successfully', task: changeTask });
 });
 
 app.delete('/tasks/:id', (req, res) => {
   const taskId = parseInt(req.params.id);
-  const index = tasks.findIndex(task => task.id === taskId);
-  if (index === -1) {
-    res.status(404).json({ error: 'Task not found' });
-  } else {
-    // Autorisierung hinzugefügt (einfaches Beispiel)
-    if (/* Überprüfung der Berechtigung hier */) {
-      tasks.splice(index, 1);
-      res.json({ result: true });
-    } else {
-      res.status(403).json({ error: 'Permission denied' });
-    }
-  }
+  tasks = tasks.filter(task => task.id !== taskId);
+  res.json({ message: 'Task deleted successfully', tasks });
 });
 
-// Verbessertes Logging für den Serverstart hinzugefügt
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Bookstore app listening on port ${port}`);
 });
